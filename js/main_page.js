f5acd2d038898bc8c836a662f0b8cd46b79dbbba
mukanov8 <mukanov.ayan@gmail.com>
2020-06-08 23:22:14 +0900

    Create main_page.js

---
+++ js/main_page.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+    var items = 0;
+    userId = getCookie("userId")
+
+
+    db.collection("items").where("userId", "==", userId).get()
+    .then((res) => {
+      res.forEach((element) => {
+        items = items+1;
+      });
+      document.getElementById("items_count").innerHTML = "Items: "+items;  
+    });
+
+    $('form').keypress(function(e) { 
+      if (e.keyCode === 13) {
+          e.preventDefault();
+          localStorage.setItem('name', 'search');
+          localStorage.setItem('searched', document.getElementById('search_bar').value.replace(/[^a-zA-Z0-9 ]/g, ""));
+          location.href = "items_page.html";
+      }
+    })
+    
+    //to change the x,y pos of div. not used currently
+    // $(function(){
+    //   var $this = $("#all");
+    //   var offset = $this.offset();
+    //   var width = $this.width();
+    //   var height = $this.height();
+     
+    //   var x = offset.left + width / 2;
+    //   var y = offset.top + height / 2;
+    //   console.log(x ,y)
+    //   var logoImg = document.getElementById('userLogo'); // or any other selector
+    //   logoImg.style.left = x-16+"px";
+    //   logoImg.style.top = y+"px";
+    //   console.log(x,y+"aaa");
+    //   console.log(userId);
+    //  })
+
+
+    all = document.getElementById("all_button");
+    t = document.getElementById("tops_button");
+    bottoms = document.getElementById("bottom_button");
+    outer = document.getElementById("outer_button");
+    shoes = document.getElementById("shoes_button");
+    add = document.getElementById("add_button");
+    all.onclick = function(){localStorage.setItem('name', 'all'); location.href = "items_page.html";};
+    t.onclick = function(){localStorage.setItem('name', 'top'); location.href = "items_page.html";};
+    bottoms.onclick = function(){localStorage.setItem('name', 'bottom'); location.href = "items_page.html";};
+    outer.onclick = function(){localStorage.setItem('name', 'outerwear');location.href = "items_page.html";};
+    shoes.onclick = function(){localStorage.setItem('name', 'shoes'); location.href = "items_page.html";};
+    add.onclick = function(){
+      add.hidden = true
+      document.getElementById("add_button1").hidden = false
+      location.href = "add_page.html";}
+});
